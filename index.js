83bcb4ddc296e8a800364f71a99f09dbb9aec7df
Alex Cwik <alexcwik1490@gmail.com>
2021-10-22 14:29:46 -0500

    added const to index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Employee = require('/lib/Employee.js');
+const Employee = require ('/Users/alexc/Documents/Team_Profile_Generator/lib/Employee.js');
+const Engineer = require ('/Users/alexc/Documents/Team_Profile_Generator/lib/Engineer.js');
+const Intern = require ('/Users/alexc/Documents/Team_Profile_Generator/lib/Intern.js');
+const Manager = require ('/Users/alexc/Documents/Team_Profile_Generator/lib/Manager.js')
+
+
